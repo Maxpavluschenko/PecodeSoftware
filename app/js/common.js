d5d6f937540a253bf39aa845585dcadba1ce1bb4
Maksym Pavlushchenko <max.pavluschenko@gmail.com>
2018-09-11 22:29:53 +0300

    create scroll

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,9 @@
-$(function() {
-
-	// Пользовательские функции
-
+type="text/javascript">
+ $(document).ready(function(){
+    $("#navbarNav").on("click","a", function (event) {
+        event.preventDefault();
+        let id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 800);
+    });
 });
--- app/js/index.js
+++
@@ -1,9 +0,0 @@
-type="text/javascript">
- $(document).ready(function(){
-    $("#navbarNavAltMarkup").on("click","a", function (event) {
-        event.preventDefault();
-        let id  = $(this).attr('href'),
-            top = $(id).offset().top;
-        $('body,html').animate({scrollTop: top}, 900);
-    });
-});
